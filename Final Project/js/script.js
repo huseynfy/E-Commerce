5ac574454f27802f59d8591ee6603eadd99c25f5
Huseyn Yusifov <55439613+HuseynF@users.noreply.github.com>
2020-03-13 00:36:51 +0400

    Add files via upload

--- Final Project/js/script.js
+++ Final Project/js/script.js
@@ -1,17 +1,60 @@
 $(document).ready(function(){
     $(".titleofcollapsible").click(function (){
-        if ($(this).next()[0] != $(".active")[0]){
-            $(".active").slideUp("slow", function() {
-                $(this).removeClass("active")
+        if ($(this).next()[0] != $(".active1")[0]){
+            $(".active1").slideUp("slow", function() {
+                $(this).removeClass("active1")
             })
             $(this).next().slideDown("slow",function(){
-                $(this).addClass("active")
+                $(this).addClass("active1")
             })
         }
         else{
-            $(".active").slideUp("slow",function(){
-                $(this).removeClass("active")
+            $(".active1").slideUp("slow",function(){
+                $(this).removeClass("active1")
             })
         }
     })
+
 })
+
+//Script for tabs
+
+let menus = document.querySelectorAll(".menu div");
+let contents = document.querySelectorAll(".contentoftab div");
+
+
+for (let menu of menus) {
+    menu.addEventListener("click", function () {
+        let active = document.querySelector(".activetab");
+        active.classList.remove("activetab");
+        this.classList.add("activetab");
+
+        let index = this.getAttribute("data-index");
+        for (let content of contents) {
+            if (content.getAttribute("data-index") == index) {
+                content.classList.remove("d-none")
+            } else {
+                content.classList.add("d-none")
+            }
+        }
+
+    })
+}
+
+
+
+ //Script of quantity increase/decrease
+function increaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+  }
+  
+  function decreaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('number').value = value;
+  }
